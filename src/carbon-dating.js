d9c73f3bd0204258c28943c896d3a366fd468805
TheCropp <pioneerpooh@gmail.com>
2020-11-01 19:39:20 +0300

    Submit

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -21,4 +21,3 @@ module.exports = function dateSample(sampleActivity) {
     return false;
   }
 };
-
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr, count = 1) {
+
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                arr = arr.flat();
+                count++;
+                return this.calculateDepth(arr, Number(count));
+            }
+        }
+
+        return count;
+    }
 };
